163a9ac4c734d3b282beeed690439b7cad7f425d
gaby-oop <gabysanz1505@gmail.com>
2021-03-18 18:27:25 +0000

    first change

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,66 @@
 import React from "react";
+import shortid from "shortid";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+//e =evento
 //create your first component
 export function Home() {
+	const [tarea, setTarea] = React.useState("");
+	const [arrayTareas, setArrayTareas] = React.useState([]);
+	const agregarTarea = e => {
+		e.preventDefault();
+		setArrayTareas([
+			...arrayTareas,
+			{
+				id: shortid.generate(),
+				nombreTarea: tarea
+			}
+		]);
+		setTarea("");
+	};
+	const eliminarTarea = id => {
+		let newlist = arrayTareas.filter(item => item.id !== id);
+		setArrayTareas(newlist);
+	};
+	// const mouseOver = () => {};
+	// const mouseOut = () => {};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center">TODOS</h1>
+			<h4 className="text-center"></h4>
+			{/* esta parte es la que agrega valor a las casillas que se van llenar */}
+			<form onSubmit={agregarTarea}>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="Ingrese Tarea"
+					onChange={e => setTarea(e.target.value)}
+					value={tarea}
+				/>
+				{/* <button
+							className="btn btn-dark btn-block"
+							type="submit">
+							Agregar
+						</button> */}
+			</form>
+			<ul className="list-group">
+				{arrayTareas.map(item => (
+					<li className="list-group-item" key={item.id}>
+						<span className="lead">
+							{item.nombreTarea}
+							<button
+								// addEventListener={() => changeColor()}
+								onClick={() => eliminarTarea(item.id)}
+								id="BtN"
+								type="button"
+								className="btn-close"
+								// data-bs-dismiss="modal"
+								aria-label="Close">
+								X
+							</button>
+						</span>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
